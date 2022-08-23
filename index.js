0544490cbdbc67ea03f771452d8bb7a9adec9deb
matialvarez7 <matias.gabriel.alvarez@gmail.com>
2022-08-22 22:55:29 -0300

    Agrego objeto matias alvarez

--- index.js
+++ index.js
@@ -6,4 +6,11 @@ const lista = [
     experienciaVersionado: true,
     dni: 29502368,
   },
+  {
+    nombre: "Matias",
+    apellido: "Alvarez",
+    experienciaJs: true,
+    experienciaVersionado: true,
+    dni: 99999999,
+  }
 ];
